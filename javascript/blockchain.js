48e1c84d88cd6f287fcbed602c4dc307f67e7c0f
Mazinhamzn <56055742+Mazinhamzn@users.noreply.github.com>
2019-12-03 21:23:10 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -7,27 +7,27 @@ var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
 function pagamentocontaEnergia() {
     var textoCampo = document.frmStatus.pagamentocontaEnergia.value;
     var pagamentocontaEnergiatx = document.getElementById("pagamentocontaEnergiatx");
-    if (textoCampo.length === 8) {
-        pagamentocontaEnergiatx.innerHTML = "Enviando transação...";
-        contrato.pagamentocontaEnergia(textoCampo)
-        .then( (transacao) => {
-            console.log("pagamentocontaEnergia - Transacao ", transacao);   
-            pagamentocontaEnergiatx.innerHTML = "Transação enviada. Aguardando processamento...";
-            transacao.wait()
-            .then( (resultado) => {
-                pagamentocontaEnergia();
-                pagamentocontaEnergiatx.innerHTML = "Transação realizada.";
+    {
+    pagamentocontaEnergiatx.innerHTML = "Enviando transação...";
+    contrato.pagamentocontaEnergia(textoCampo)
+    .then( (transacao) => {
+        console.log("pagamentocontaEnergia - Transacao ", transacao);   
+        pagamentocontaEnergiatx.innerHTML = "Transação enviada. Aguardando processamento...";
+        transacao.wait()
+    .then( (resultado) => {
+        pagamentocontaEnergia();
+        pagamentocontaEnergiatx.innerHTML = "Transação realizada.";
             })       
-            .catch( (err) => {
-                console.error("pagamentocontaEnergia - Aguardando tx ser minerada");
-                console.error(err);
-                pagamentocontaEnergiatx.innerHTML = "Algo saiu errado: " + err.message;
+    .catch( (err) => {
+        console.error("pagamentocontaEnergia - Aguardando tx ser minerada");
+        console.error(err);
+        pagamentocontaEnergiatx.innerHTML = "Algo saiu errado: " + err.message;
             });
         })
-            .catch( (err) => {
-                 console.error("pagamentocontaEnergia");
-                console.error(err);
-                pagamentocontaEnergiatx.innerHTML = "Algo saiu errado: " + err.message;
+    .catch( (err) => {
+        console.error("pagamentocontaEnergia");
+        console.error(err);
+        pagamentocontaEnergiatx.innerHTML = "Algo saiu errado: " + err.message;
         });
     }
 
@@ -35,40 +35,26 @@ function pagamentocontaEnergia() {
 function enviaEther() {
     var textoCampo = document.frmStatus.enviaEthertx.value;
     var enviaEthertx = document.getElementById("enviaEthertx");
-    if (textoCampo.length === 8) {
-        enviaEthertx.innerHTML = "Enviando transação...";
-        contrato.enviaEther(textoCampo)
-        .then( (transacao) => {
-            console.log("enviaEther - Transacao ", transacao);   
-            enviaEthertx.innerHTML = "Transação enviada. Aguardando processamento...";
-            transacao.wait()
-            .then( (resultado) => {
-                enviaEther();
-                enviaEthertx.innerHTML = "Transação realizada.";
+    enviaEthertx.innerHTML = "Enviando transação...";
+    contrato.enviaEther(textoCampo)
+    .then( (transacao) => {
+        console.log("enviaEther - Transacao ", transacao);   
+        enviaEthertx.innerHTML = "Transação enviada. Aguardando processamento...";
+        transacao.wait()
+    .then( (resultado) => {
+        enviaEther();
+        enviaEthertx.innerHTML = "Transação realizada.";
             })       
-            .catch( (err) => {
-                console.error("enviaEther - Aguardando tx ser minerada");
-                console.error(err);
-                enviaEthertx.innerHTML = "Algo saiu errado: " + err.message;
+    .catch( (err) => {
+        console.error("enviaEther - Aguardando tx ser minerada");
+        console.error(err);
+        enviaEthertx.innerHTML = "Algo saiu errado: " + err.message;
             });
         })
-            .catch( (err) => {
-                 console.error("enviaEther");
-                console.error(err);
-                enviaEthertx.innerHTML = "Algo saiu errado: " + err.message;
-        });
-    }
+               
 }
     
 function registrarConsumoMensal () {
     var caixaConsumoMensalTx = document.getElementById("caixaConsumoMensalTx");     
-    contrato.consumoMensal()
-    .then( (consumoMensal) => {
-        caixaConsumoMensalTx.innerHTML = consumoMensal;
-    })
-    .catch( (err) => {
-        console.error(err);
-        caixaConsumoMensalTx.innerHTML = err;
-    });
-}
+    caixaConsumoMensalTx.value = 20000000000000000;    
 }
